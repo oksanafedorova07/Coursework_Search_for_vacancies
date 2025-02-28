51c2c8097f63b38a115b44882722f2c7214044ab
oksanafedorova07 <kcenka4@gmail.com>
2025-02-28 23:15:34 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from src.hh_api import HeadHunterAPI
+from src.vacancy import Vacancy
+from src.json_saver import JSONSaver
+
+def user_interaction():
+    hh_api = HeadHunterAPI()
+    search_query = input("Введите поисковый запрос: ")
+    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+
+    hh_vacancies = hh_api.get_vacancies(search_query)
+    vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
+
+    # Фильтрация вакансий с проверкой на None
+    filtered_vacancies = [
+        v for v in vacancies_list
+        if v.description is not None and all(word in v.description for word in filter_words)
+    ]
+
+    sorted_vacancies = sorted(filtered_vacancies, reverse=True)
+    top_vacancies = sorted_vacancies[:top_n]
+
+    json_saver = JSONSaver()
+    for vacancy in top_vacancies:
+        json_saver.add_vacancy(vacancy)
+
+    for vacancy in top_vacancies:
+        print(f"{vacancy.name} - {vacancy.salary} - {vacancy.url}")
+
+if __name__ == "__main__":
+    user_interaction()
