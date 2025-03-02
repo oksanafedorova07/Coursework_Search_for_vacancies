5db197eab776d21920739047bc285a923cd64743
oksanafedorova07 <kcenka4@gmail.com>
2025-03-02 22:32:36 +0300

    correct hh_api.py

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,20 +1,44 @@
 from abc import ABC, abstractmethod
-
+from typing import List, Dict
 import requests
 
 
 class VacancyAPI(ABC):
+    """Абстрактный класс для работы с API сервисов с вакансиями."""
+
     @abstractmethod
-    def get_vacancies(self, query):
+    def get_vacancies(self, query: str) -> List[Dict]:
+        """Получает список вакансий по ключевому слову."""
         pass
 
 
 class HeadHunterAPI(VacancyAPI):
-    def get_vacancies(self, query):
-        url = "https://api.hh.ru/vacancies"
-        params = {"text": query, "area": 113, "per_page": 100}
-        response = requests.get(url, params=params)
+    """
+    Класс для работы с API HeadHunter.
+    Реализует метод для получения вакансий с сайта hh.ru.
+    """
+
+    def __init__(self) -> None:
+        """
+        Инициализирует экземпляр класса HeadHunterAPI.
+        Устанавливает базовый URL и параметры запроса.
+        """
+        self._url = "https://api.hh.ru/vacancies"  # Базовый URL API
+        self._params = {"per_page": 100, "area": "113"}  # Параметры запроса по умолчанию
+
+    def get_vacancies(self, query: str) -> List[Dict]:
+        """
+        Получает список вакансий с сайта hh.ru по ключевому слову.
+
+        :param query: Ключевое слово для поиска вакансий.
+        :return: Список вакансий в формате словарей.
+        :raises Exception: Если запрос к API завершился с ошибкой.
+        """
+        self._params["text"] = query  # Добавляем ключевое слово в параметры запроса
+        response = requests.get(self._url, params=self._params)
+
+        # Проверяем статус ответа
         if response.status_code == 200:
-            return response.json()["items"]
+            return response.json()["items"]  # Возвращаем список вакансий
         else:
             raise Exception(f"Ошибка при запросе к API: {response.status_code}")
